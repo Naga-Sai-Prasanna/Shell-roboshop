ad3577eeaa6d625e7ef71041305c8ce8ed138680
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-02-24 17:26:32 +0530

    message

--- test.sh
+++ test.sh
@@ -1,3 +1,22 @@
-#!/bin/bash
+# #!/bin/bash
 
-echo "please enter your name: "
+# # echo "please enter your name: "
+# # read name
+
+# # echo "$name is best girl"
+
+
+# numbers=("a" "b" "c" "d" "n")  
+
+# =${numbers[-1]}
+
+# echo "the number is $last_value"
+
+
+
+my_array=("apple" "banana" "orange" "grape")
+
+# Access the last element
+last_value="${my_array[-1]}"
+
+echo "The last value is: $last_value"
